11ae41e992579c3a6356343eb1b01c2460ab7dc5
lkimihol <lkimilhol@gmail.com>
2021-10-16 17:04:29 +0900

    refactor(ProducerApplication): 카프카 produce 예제 작성

--- producer/src/main/kotlin/com/example/producer/ProducerApplication.kt
+++ producer/src/main/kotlin/com/example/producer/ProducerApplication.kt
@@ -1,11 +1,20 @@
 package com.example.producer
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class ProducerApplication
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import java.util.concurrent.Future
 
 fun main(args: Array<String>) {
-    runApplication<ProducerApplication>(*args)
+    var producerRecord : ProducerRecord<String, String> = ProducerRecord("test", "key", "hello!!!")
+    val map = mutableMapOf<String, String>()
+    map["key.serializer"]   = "org.apache.kafka.common.serialization.StringSerializer"
+    map["value.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+    map["bootstrap.servers"] = "localhost:9092"
+    var producer = KafkaProducer<String, String>(map as Map<String, Any>?)
+
+    for (i in 1..10) {
+        var future: Future<RecordMetadata> = producer.send(producerRecord)!!
+        future.get()
+    }
 }
