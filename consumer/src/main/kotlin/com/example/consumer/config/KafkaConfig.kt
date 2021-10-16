32d4aebc9c4cc2e04280bdc9d41ac6b741ad6fe1
lkimihol <lkimilhol@gmail.com>
2021-10-16 11:46:18 +0900

    refactor(KafkaConfig,SampleTopicListener): 패키지 경로 수정

--- consumer/src/main/kotlin/com/example/consumer/config/KafkaConfig.kt
+++ consumer/src/main/kotlin/com/example/consumer/config/KafkaConfig.kt
@@ -1,4 +1,4 @@
-package com.example.kotlinkafkatoy.config
+package com.example.consumer.config
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
--- consumer/src/main/kotlin/com/example/consumer/service/SampleTopicListener.kt
+++ consumer/src/main/kotlin/com/example/consumer/service/SampleTopicListener.kt
@@ -1,13 +1,14 @@
-package com.example.kotlinkafkatoy.service
+package com.example.consumer.service
 
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
-import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 
-@Component
+@Service
 class SampleTopicListener {
-    val log = LoggerFactory.getLogger(SampleTopicListener::class.java)
+    val log: Logger = LoggerFactory.getLogger(SampleTopicListener::class.java)
 
     @KafkaListener(topics = ["test"])
     fun consume(@Payload data: String) {
