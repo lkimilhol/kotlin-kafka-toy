ceb6e10be3f37295456742c8bbc0c7c1a4f1eaf0
lkimilhol <lkimilhol@gmail.com>
2021-11-07 10:10:30 +0900

    test(ConsumerTest): 테스트 작성

---
+++ src/test/kotlin/com/example/kafkatoy/service/ConsumerTest.kt
@@ -0,0 +1,16 @@
+package com.example.kafkatoy.service
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.kafka.test.context.EmbeddedKafka
+
+class ConsumerTest {
+    @Test
+    fun `토픽_쌓기_테스트`() {
+        // given
+
+        // when
+
+        // then
+    }
+}
