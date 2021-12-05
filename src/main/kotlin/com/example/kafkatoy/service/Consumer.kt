4cdbbfcd490c05995ece46eb71333a258226b982
lkimihol <lkimilhol@gmail.com>
2021-12-05 17:11:09 +0900

    refactor(Consumer,Producer): 블로깅을 위한 코드 수정

--- src/main/kotlin/com/example/kafkatoy/service/Consumer.kt
+++ src/main/kotlin/com/example/kafkatoy/service/Consumer.kt
@@ -5,21 +5,26 @@ import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.BatchListenerFailedException
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
 import java.util.concurrent.CountDownLatch
 
 @Service
 class Consumer {
-
     private var messageList = listOf<KafkaMessage>()
 
     @KafkaListener(id = "kafkaMessages", topics = ["test"], containerFactory = "kafkaListenerContainerFactory")
     fun consume(kafkaMessages: ConsumerRecords<String, KafkaMessage>) {
+        throw BatchListenerFailedException("failed", 0)
         kafkaMessages.forEach { println(it) }
     }
 
     fun getPayload(): List<KafkaMessage> {
         return messageList
+    }
+
+    fun consumeDLT() {
+
     }
 }
--- src/main/kotlin/com/example/kafkatoy/service/Producer.kt
+++ src/main/kotlin/com/example/kafkatoy/service/Producer.kt
@@ -1,13 +1,13 @@
 package com.example.kafkatoy.service
 
 import com.example.kafkatoy.domain.KafkaMessage
-import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
 @Service
-class Producer(private val kafkaProducerTemplate: ReactiveKafkaProducerTemplate<String, KafkaMessage>) {
+class Producer(private val kafkaProducerTemplate: KafkaTemplate<String, KafkaMessage>) {
 
     fun produce(topic: String, kafkaMessage: KafkaMessage) {
-        kafkaProducerTemplate.send(topic, kafkaMessage).subscribe()
+        kafkaProducerTemplate.send(topic, kafkaMessage)
     }
 }
