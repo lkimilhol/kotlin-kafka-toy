4ff4ca4937ae803eb1506dcdbeddf2ea24dd87d7
ilho-kim <ilho.kim@myrealtrip.com>
2022-10-13 10:34:09 +0900

    예제 수정

--- src/main/kotlin/com/example/kafkatoy/controller/ConsumerController.kt
+++ src/main/kotlin/com/example/kafkatoy/controller/ConsumerController.kt
@@ -1,13 +1,14 @@
 package com.example.kafkatoy.controller
 
 import com.example.kafkatoy.service.DLTConsumer
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ConsumerController(private val consumer: DLTConsumer) {
 
-    @PostMapping("/")
+    @GetMapping("/")
     fun test() {
         consumer.dltConsume()
     }
--- src/main/kotlin/com/example/kafkatoy/controller/ProducerController.kt
+++ src/main/kotlin/com/example/kafkatoy/controller/ProducerController.kt
@@ -3,12 +3,13 @@ package com.example.kafkatoy.controller
 import com.example.kafkatoy.domain.KafkaMessage
 import com.example.kafkatoy.service.Producer
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ProducerController(private val producer: Producer) {
 
-    @GetMapping("/")
+    @PostMapping("/")
     fun test() {
         producer.produce("test", KafkaMessage(1L, "test"))
     }
